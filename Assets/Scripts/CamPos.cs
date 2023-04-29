5e5b5c5ee50df20c92909385caccfcd2a95dc5b5
js0753 <jainam728@gmail.com>
2023-04-29 10:55:27 -0400

    Initial commit

---
+++ Assets/Scripts/CamPos.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamPos : MonoBehaviour
+{
+    public Camera playerCam;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 camPos = playerCam.transform.position;
+        Vector3 playerPos = Player.transform.position;
+        Vector3 gap = new Vector3(-0.1f, 1.12f, -2.085f);
+        playerCam.transform.position = playerPos + gap;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 camPos = playerCam.transform.position;
+        Vector3 playerPos = Player.transform.position;
+        Vector3 gap = new Vector3(-0.1f, 1.12f, -2.085f);
+        playerCam.transform.position = playerPos + gap;
+
+    }
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public GameObject playerModel;
+    public Vector3 unitForward;
+    public Vector3 unitBackward;
+    public Vector3 unitLeft;
+    public Vector3 unitRight;
+    public float verticalVelocity;
+    public float upwardForce;
+    float gravity = -0.0098f ;
+    void Start()
+    {
+        unitForward = new Vector3(0,0,0.01f);
+        unitBackward= new Vector3(0,0,-0.01f);
+        unitLeft = new Vector3(-0.01f, 0, 0);
+        unitRight = new Vector3(0.01f, 0, 0);
+        verticalVelocity = 0.0f;
+        upwardForce = 0.0f;
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        verticalVelocity += (upwardForce + gravity) * Time.deltaTime;
+        //bool hasColliderAtNewPos = Physics.OverlapSphere(playerModel.transform.position + new Vector3(0.0f, verticalVelocity,0.0f), 0.1f).Length > 0;
+        Collider[] colliders = Physics.OverlapBox(playerModel.transform.position + new Vector3(0.0f, verticalVelocity, 0.0f), new Vector3(0.5f,0.5f,0.5f) / 2f);
+        bool hasColliderAtNewPos = false;
+        foreach (Collider collider in colliders)
+        {
+            // Check if the collider has a specific tag
+            if (collider.CompareTag("Player")==false)
+            {
+                // Set the flag to true and exit the loop
+                hasColliderAtNewPos = true;
+                break;
+            }
+        }
+        if (hasColliderAtNewPos)
+        {
+            Debug.Log("On Floor!");
+            verticalVelocity = 0.0f;
+            upwardForce = -gravity;
+
+        }
+        else
+        {
+            upwardForce = 0.0f;
+        }
+        
+        playerModel.transform.position = playerModel.transform.position + new Vector3(0.0f, verticalVelocity, 0.0f);
+        if (Input.GetButton("Forward")) {
+            playerModel.transform.position += unitForward;
+        }
+        if (Input.GetButton("Backward"))
+        {
+            playerModel.transform.position += unitBackward;
+        }
+        if (Input.GetButton("Left"))
+        {
+            playerModel.transform.position += unitLeft;
+        }
+        if (Input.GetButton("Right"))
+        {
+            playerModel.transform.position += unitRight;
+        }
+        
+    }
+}
